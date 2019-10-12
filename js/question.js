1cb4980621ababa35d4be2b4dd8b96ffcc4e060d
jsalazaruh <jonathansalazar94@gmail.com>
2019-10-12 18:31:19 -0500

    adding creating of questions

--- js/question.js
+++ js/question.js
@@ -5,13 +5,23 @@ var sessTime = localStorage.getItem("sessionTime");
 
 var max = parseInt(upRange);
 var min = 0;
+var numQuest = questiNum * 2;
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function creatingQuest(questiNum) {
+  var quest = new Array();
+  for(i = 0; i < questiNum; i++) {
+    console.log("Question: " + i);
+    quest[i] = getRandomInt(min, max);
 
-console.log(getRandomInt(min,max));
+  }
+  return ;
+}
+
+// console.log(getRandomInt(min,max));
 // console.log(firstOperation);
 // console.log(upRange);
 // console.log(questiNum);

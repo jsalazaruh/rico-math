77afea3b759c860da596b3041e1c39ee14bcfe72
jsalazaruh <jonathansalazar94@gmail.com>
2019-10-13 16:45:38 -0500

    solved reference error

--- js/question.js
+++ js/question.js
@@ -6,6 +6,11 @@ var sessTime = localStorage.getItem("sessionTime");
 var max = parseInt(upRange);
 var min = 0;
 
+$(function() {
+    getQuestion();
+});
+
+
 function setOperator(firstOperation) {
   if(firstOperation === "ADD")
     return '+';
@@ -23,49 +28,47 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-$(function() {
-    getQuestion();
-});
+  function getQuestion() {
+    var count = 0;
+    var results = [];
+    while(count < questiNum) {
+      var container = $('<div />');
+      var operators = setOperator(firstOperation);
+      var val1 = getRandomInt(min,max);
+      var val2 = getRandomInt(min,max);
+      var lbl = $('<label />');
+      lbl.html(val1 + ' ' + operator + ' ' + val2 + ' = ');
+      container.append(lbl);
+      var input = $('<input type="text" />');
+      container.append(input);
+      var btn = $('<input type="button" value="Submit result" />');
+      var val;
+      count++;
+      btn.click(function() {
 
-function getQuestion() {
-  var count = 0,
-  results = [];
-  while(count < questiNum) {
-    var container = $('<div />');
-    var operators = setOperator(firstOperation);
-    var val1 = getRandomInt(min,max);
-    var val2 = getRandomInt(min,max);
-    var lbl = $('<label />');
-    lbl.html(val1 + ' ' + operator + ' ' + val2 + ' = ');
-    container.append(lbl);
-    var input = $('<input type="text" />');
-    container.append(input);
-    var btn = $('<input type="button" value="Submit result" />');
-    var val;
-    count++;
-    btn.click(function() {
+          results.push({
+              number1 : val1,
+              number2 : val2,
+              answer : input.val()
+          });
 
-        results.push({
-            number1 : val1,
-            number2 : val2,
-            answer : input.val()
-        });
+          input.attr('disabled', true);
+          $(this).attr('disabled', true);
 
-        input.attr('disabled', true);
-        $(this).attr('disabled', true);
+          $(this).after(function() {
+              if(eval(val1 + operator + val2) == input.val()) return 'RIGHT';
+              return 'WRONG';
+          });
 
-        $(this).after(function() {
-            if(eval(val1 + operator + val2) == input.val()) return 'RIGHT';
-            return 'WRONG';
-        });
+          getQuestion();
 
-        getQuestion();
-
-    });
-    container.append(btn);
-    $('body').append(container);
+      });
+      container.append(btn);
+      $('body').append(container);
+    }
   }
-}
+
+
 
 // function updateResults() {
 //     $('#json').html(JSON.stringify(results));

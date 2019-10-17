24e20b8968a78ed6a3a8a9e16f82481f47a939b9
jsalazaruh <jonathansalazar94@gmail.com>
2019-10-17 16:10:29 -0500

    fixed css

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,12 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("numRange");
+
 $(function() {
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("numRange");
+  document.getElementById("result").innerHTML=localStorage.getItem("operation");
+  output.innerHTML = slider.value;
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
   $("#numberOfQuestions").on('input', function() {
     var questNum = $(this).val();
     localStorage.setItem("questionNumber", questNum);
@@ -14,8 +20,3 @@ $(function() {
     localStorage.setItem("range", range);
   });
 });
-output.innerHTML = slider.value;
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-document.getElementById("result").innerHTML=localStorage.getItem("operation");
